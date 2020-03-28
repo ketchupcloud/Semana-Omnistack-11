4deef2cf80c3dda3af3e80039bc724a70e84b4fd
unknown <juliapohlmann@hotmail.com>
2020-03-27 23:17:21 -0300

    +testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333); //rota do nosso app
+//esse errors do celebrate torna as mensagens de erro de autenticação 
+//  mais user friendly
+
+module.exports = app;
